3a2942ce18c1c074a75b98e7132edd24fc7a7ac4
Lucas97450 <lucastionohoue.pro@gmail.com>
2025-10-21 10:06:55 -0400

    Apply dark green-on-black theme: Consolas font, lime text, flat green button, black background

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,18 +29,19 @@ namespace patch_seb_lucas
 			this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
 			| System.Windows.Forms.AnchorStyles.Right)));
 			this.label1.AutoSize = true;
-			this.label1.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Font = new System.Drawing.Font("Consolas", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
 			this.label1.Location = new System.Drawing.Point(180, 9);
 			this.label1.Name = "label1";
 			this.label1.Size = new System.Drawing.Size(212, 23);
 			this.label1.TabIndex = 0;
 			this.label1.Text = "Safe Exam Browser Patch";
+			this.label1.ForeColor = System.Drawing.Color.Lime;
 			this.label1.Click += new System.EventHandler(this.label1_Click);
 			
 			// textBox1
 			this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
 			| System.Windows.Forms.AnchorStyles.Right)));
-			this.textBox1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.textBox1.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
 			this.textBox1.Location = new System.Drawing.Point(12, 50);
 			this.textBox1.Multiline = true;
 			this.textBox1.Name = "textBox1";
@@ -48,6 +49,8 @@ namespace patch_seb_lucas
 			this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
 			this.textBox1.Size = new System.Drawing.Size(547, 196);
 			this.textBox1.TabIndex = 1;
+			this.textBox1.BackColor = System.Drawing.Color.Black;
+			this.textBox1.ForeColor = System.Drawing.Color.Lime;
 			
 			// button1
 			this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
@@ -56,8 +59,13 @@ namespace patch_seb_lucas
 			this.button1.Name = "button1";
 			this.button1.Size = new System.Drawing.Size(170, 46);
 			this.button1.TabIndex = 2;
-			this.button1.Text = "PATCH";
-			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Text = "LET'S PATCH!!!";
+			this.button1.UseVisualStyleBackColor = false;
+			this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button1.FlatAppearance.BorderSize = 1;
+			this.button1.FlatAppearance.BorderColor = System.Drawing.Color.Lime;
+			this.button1.BackColor = System.Drawing.Color.Black;
+			this.button1.ForeColor = System.Drawing.Color.Lime;
 			this.button1.Click += new System.EventHandler(this.button1_Click);
 			
 			// checkBox1
@@ -69,7 +77,9 @@ namespace patch_seb_lucas
 			this.checkBox1.Size = new System.Drawing.Size(71, 21);
 			this.checkBox1.TabIndex = 3;
 			this.checkBox1.Text = "Backup";
-			this.checkBox1.UseVisualStyleBackColor = true;
+			this.checkBox1.UseVisualStyleBackColor = false;
+			this.checkBox1.BackColor = System.Drawing.Color.Black;
+			this.checkBox1.ForeColor = System.Drawing.Color.Lime;
 			
 			// checkBox2
 			this.checkBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
@@ -82,7 +92,9 @@ namespace patch_seb_lucas
 			this.checkBox2.Size = new System.Drawing.Size(125, 21);
 			this.checkBox2.TabIndex = 4;
 			this.checkBox2.Text = "Install Certificate";
-			this.checkBox2.UseVisualStyleBackColor = true;
+			this.checkBox2.UseVisualStyleBackColor = false;
+			this.checkBox2.BackColor = System.Drawing.Color.Black;
+			this.checkBox2.ForeColor = System.Drawing.Color.Lime;
 			
 			// Form1
 			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
@@ -93,7 +105,7 @@ namespace patch_seb_lucas
 			this.Controls.Add(this.button1);
 			this.Controls.Add(this.textBox1);
 			this.Controls.Add(this.label1);
-			this.Font = new System.Drawing.Font("Segoe UI", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.Font = new System.Drawing.Font("Consolas", 8.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
 			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
 			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
 			this.MaximizeBox = false;
@@ -101,6 +113,7 @@ namespace patch_seb_lucas
 			this.Name = "Form1";
 			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
 			this.Text = "Safe Exam Browser Patch";
+			this.BackColor = System.Drawing.Color.Black;
 			this.Load += new System.EventHandler(this.Form1_Load);
 			this.ResumeLayout(false);
 			this.PerformLayout();
